f366aeff36e704ef18ba4c52e64354e01a92b7f4
jkalogero <johnkalo22@hotmail.com>
2020-08-08 01:48:56 +0300

    Implement basic backend API.

---
+++ Backend/run.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from flask import render_template
+from flask import redirect, url_for
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+from bson import json_util
+import json
+from pprint import pprint
+from bson.son import SON
+import time
+# from rest_apis import main
+
+# instantiate the app
+app = Flask(__name__)
+# setup mongo
+app.config["MONGO_URI"] = "mongodb://localhost:27017/MediCom_db"
+mongo = PyMongo(app)
+
+
+# configuration
+DEBUG = True
+app.config.from_object(__name__)
+app.url_map.strict_slashes = False #ignore trailing slashes
+
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+# app.register_blueprint(main)
+
+@app.route('/condition')
+def getCondition():
+    condition=request.args.get('condition')
+    print(condition)
+    # pipeline = [{"$match":{"condition":condition }},{"$unwind": "$intervention"}, {"$group": {"_id": "$intervention", "count": {"$sum": 1}}}, {"$sort": SON([("count", -1), ("_id", 1)])}]
+    pipeline = [{"$match":{"condition": condition}}]
+    sortedResults = list(mongo.db.MediCom.aggregate(pipeline))
+    pprint(sortedResults)
+    return json.dumps(sortedResults,default=str)
+
+if __name__ == '__main__':
+    app.run()
